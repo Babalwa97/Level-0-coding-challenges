f2d3bfd29bccc7d45466929eefcf8db002e05412
Babalwa97 <89903009+Babalwa97@users.noreply.github.com>
2021-09-20 23:11:56 +0200

    Update task0.6.js

--- task0.6.js
+++ task0.6.js
@@ -1,13 +1,13 @@
-function maximumNumber(a, b, c) { 
-  var a = 11;
-  var b = 22;
-  var c = 6;
+function maximumNumber(num1, num2, num3) { 
+  var num1 = 11;
+  var num2 = 22;
+  var num3 = 6;
   
-  if ((a > b) & (a > c)) {
-    return a;
-  } else if ((b > c) & (b > a)) {
-    return b;
-  } else if ((c > a) & (c > b)) {
-    return c;
+  if ((num1 > num2) & (num1 > num3)) {
+    return num1;
+  } else if ((num2 > num3) & (num2 > num1)) {
+    return num2;
+  } else if ((num3 > num1) & (num3 > num2)) {
+    return num3;
   }
 }
