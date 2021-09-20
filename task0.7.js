81bb468a94a1e7adbc6065b641875fa233c87670
Babalwa97 <89903009+Babalwa97@users.noreply.github.com>
2021-09-20 23:22:47 +0200

    Update task0.7.js

--- task0.7.js
+++ task0.7.js
@@ -1,17 +1,16 @@
-var a = 70; // Temperature of choice
-var b = a * (9 / 5) + 32; // To convert Celsius to Fahrenheit
-var c = a + "\xB0C is " + b + " \xB0F.";
-
-function celToFah() {
-  console.log(c);
+function celsiusToFahrenheit() {
+  var temp = 70;
+  var convert = temp * (9 / 5) + 32;
+  var answer = temp + "\xB0C is " + convert + " \xB0F."; {
+  return answer;
+ }
 }
-console.log(celToFah());
 
-var d = 80; // Temperature of choice
-var e = (d - 32) * (5 / 9); // To convert Fahrenheit to Celsius
-var f = d + "\xB0F is " + e + " \xB0C.";
 
-function fahToCel() {
-  console.log(f);
+function fahrenheitToCelsius() { 
+  var temperature = 80;
+  var convert = (temperature - 32) * (5 / 9);
+  var answer = temperature + "\xB0F is " + convert + " \xB0C."; {
+  return answer;
+ }
 }
-console.log(fahToCel());
