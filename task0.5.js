9e9a2819776cf088f522d4d27afa14a6e4b90c8c
Babalwa97 <89903009+Babalwa97@users.noreply.github.com>
2021-09-20 22:58:52 +0200

    Update task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,10 +1,8 @@
 var a = 7;
 var b = 5;
 var c = 8;
-var s = (a + b + c) / 2; // s is a semiperimeter
-var Area = (s * (s - a) * (s - b) * (s - c)) ** 0.5; // To calculate the area of a triangle using sides
+var s = (a + b + c) / 2;
+var Area = (s * (s - a) * (s - b) * (s - c)) ** 0.5; 
 function triangleArea(a, b, c) {
   return Area;
 }
-
-console.log(triangleArea(a, b, c));
