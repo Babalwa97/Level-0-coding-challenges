3e4b6b137071c33ab45e9cbcfe694d0ef1808e33
Babalwa97 <89903009+Babalwa97@users.noreply.github.com>
2021-09-21 11:17:33 +0200

    Update task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,8 +1,5 @@
  
-function triangleArea(a, b, c) {
-  var side1 = 7;
-  var side2 = 5;
-  var side3 = 8;
+function triangleArea(side1, side2, side3) {
   var s = (side1 + side2 + side3) / 2;
   var Area = (s * (s - side1) * (s - side2) * (s - side3)) ** 0.5; {
    return Area; 
